ebbdc5723d4dc71316a010f26f61cb60f0ead90a
Hajo Kleingeld <hajokleingeld@gmail.com>
2016-12-09 14:44:16 +0100

    Added functions to extract several features from a uint16 array.

---
+++ Firmware/features/Features.c
@@ -0,0 +1,110 @@
+/*
+ * Features.c
+ *
+ * Created: 9-12-2016 12:07:56
+ *  Author: Hajo
+ */
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+
+
+uint16_t Minimum(uint16_t size, uint16_t * array){
+	uint16_t min = USHRT_MAX;
+	
+	for(int i = 0; i < size ;i++){
+		if(array[i] < min){
+			min = array[i];
+		}
+	}
+		
+	return(min);
+}
+
+uint16_t Maximum(uint16_t size, uint16_t * array){
+	uint16_t max = 0;
+	
+	for(int i = 0; i < size ; i++){
+		if(array[i] < max){
+			max = array[i];
+		}
+	}
+	
+	return(max);
+}
+
+uint16_t Average(uint16_t size, uint16_t * array){
+	uint32_t sum = 0;
+	
+	for(int i = 0; i < size ; i++){
+		sum += array[i];
+	}
+	
+	return(sum/size);
+}
+
+uint16_t AbsoluteDifference(uint16_t size, uint16_t * array1, uint16_t * array2){
+	uint32_t AbsDiff = 0;
+	
+	for(int i = 0; i < size ; i++){
+		//I'm allowed to cast the uint16 numbers to int as their maximum value is 3500
+		AbsDiff += abs((int32_t) array1[i] - (int32_t) array2[i]);
+	}
+	
+	return(AbsDiff);
+}
+
+uint16_t PulseLenght(uint16_t size, uint16_t * array){
+	uint16_t prev = array[0];
+	uint16_t StartPulse = 0;
+	
+	for(int i = 1; i < size; i++){
+		//I'm allowed to cast the uint16 numbers to int as their maximum value is 3500
+		int16_t diff = (int16_t) prev - (int16_t) array[i];
+		if(diff > 2){
+			if(StartPulse == 0){
+				StartPulse = i;
+			}
+			continue;
+		}
+		
+		if(StartPulse != 0){
+			return(i - StartPulse);
+		}
+	}
+	return 0;
+}
+
+uint16_t PulseStart(uint16_t size, uint16_t threshold, uint16_t * array){
+	uint16_t prev = array[0];
+	
+	for(int i = 1; i < size; i++){
+		//I'm allowed to cast the uint16 numbers to int as their maximum value is 3500
+		int16_t diff = (int16_t) prev - (int16_t) array[i];
+		if(diff > threshold){
+			return i;
+		}
+	}
+	return 0;
+}
+
+uint16_t PulseEnd(uint16_t size, uint16_t threshold, uint16_t * array){
+	uint16_t prev = array[0];
+	uint16_t StartPulse = 0;
+	
+	for(int i = 1; i < size; i++){
+		//I'm allowed to cast the uint16 numbers to int as their maximum value is 3500
+		int16_t diff = (int16_t) prev - (int16_t) array[i];
+		if(diff > threshold){
+			if(StartPulse == 0){
+				StartPulse = i;
+			}
+			continue;
+		}
+		
+		if(StartPulse != 0){
+			return(i);
+		}
+	}
+	return 0;
+}
---
+++ Firmware/features/Features.h
@@ -0,0 +1,24 @@
+/*
+ * Features.h
+ *
+ * Created: 9-12-2016 12:08:11
+ *  Author: Hajo
+ *
+ * Contains several functions to get certain features from uint16 arrays
+ */
+#ifndef _FEATURES_H_GUARD_
+#define _FEATURES_H_GUARD_
+
+#include <stdint.h>
+
+uint16_t Minimum(uint16_t size, uint16_t * array);
+uint16_t Maximum(uint16_t size, uint16_t * array);
+uint16_t Average(uint16_t size, uint16_t * array);
+
+uint16_t AbsoluteDifference(uint16_t size, uint16_t * array1, uint16_t * array2);
+
+uint16_t PulseLenght(uint16_t size, uint16_t * array);
+uint16_t PulseStart(uint16_t size, uint16_t threshold, uint16_t * array);
+uint16_t PulseEnd(uint16_t size, uint16_t threshold, uint16_t * array);
+
+#endif
