6c6b18c1945ebe032f5ca3f206cd433b986cfe7a
Hajo Kleingeld <hajokleingeld@gmail.com>
2017-06-22 13:15:22 +0200

    Added requirements and removed unnecessary comments.

--- Communiation/Communiation/src/main.c
+++ Communiation/Communiation/src/main.c
@@ -1,33 +1,28 @@
-/**
- * \file
+/********************
+ * Specs:
  *
- * \brief Empty user application template
+ * Dark Sense
+ *		ADC Burst mode (250 Khz) for 50 samples (1/(250*10^3)*50 = 0.2 ms, where the light is only one for approx 0.1 ms)
+ *		Filters to make the data interpretable
+ *		Transmission of data via uart
+ *		BONUS: Run algorithm on platform
+ * 
+ * Superblink
+ *		Superblink port with Constant sync between blink and ADC Burst mode.
+ *		Build RX receiver to interpret super blink commands.
+ * 
+ * Communication
+ *		Slow continuous ADC mode (100 Khz)
+ *		Invent some kind of pulse detection
+ *		Modify superblink to be able to transmit data.
  *
- */
+ *
+ * Not filled means TODO
+ * o Means Work in progress
+ * ? means done but needs to be tested
+ * X = Completed and tested
+ *************************************************/
 
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
-/*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 #include <asf.h>
 #include <interrupt.h>
 #include <stdlib.h>
@@ -42,11 +37,6 @@ uint8_t uart_sending = 0;
 uint8_t adc_working = 1;
 
 char itoabuffer[20];
-
-/*
- * 41 Mhz gives a nice round prescale value of 84/6 = 14. We want a ADC frequency of 250Khz
- * So there are 6000/250 = 24 cycles in a single ADC conversion.
- */
 
 void uart_start_tx(void){
 	if(!uart_sending){
@@ -86,6 +76,10 @@ uint8_t uart_send_string(const char * string){
 
 #define ADC_CLOCK 6000000 
 
+/*
+ * 41 Mhz gives a nice round prescale value of 84/6 = 14. We want a ADC frequency of 250Khz
+ * So there are 6000/250 = 24 cycles in a single ADC conversion.
+ */
 void adc_setup(void)
 {
 	pmc_enable_periph_clk(ID_ADC);
@@ -99,10 +93,6 @@ void adc_setup(void)
 	
 	//adc_configure_trigger(ADC, ADC_TRIG_TIO_CH_2, 0);		//Start ADC conversions on Timer II (TODO).
 	
-// 	ADC->ADC_MR = ADC_MR_FWUP | ADC_MR_PRESCAL(1) | ADC_MR_STARTUP_SUT16 | ADC_MR_SETTLING_AST9 | ADC_MR_TRACKTIM(9) | ADC_MR_TRANSFER(9);
-// 	ADC->ADC_CHER = ADC_CHER_CH0;
-// 	ADC->ADC_IER = ADC_IER_DRDY;
-	
 	adc_fifo = FIFOInit(1000);
 }
 
@@ -110,10 +100,9 @@ void uart_setup(void){
 	pmc_enable_periph_clk(ID_UART);
 	
 	UART->UART_MR = UART_MR_PAR_NO | UART_MR_CHMODE_NORMAL;	//no parity, no fancy test setups.	
-	//UART->UART_IER = UART_IER_TXEMPTY;
 	UART->UART_IER = UART_IER_RXRDY;			            // enable rx
 	UART->UART_BRGR = 46;								    // set boudrate to 115200 boud
-	UART->UART_CR = UART_CR_RXEN | UART_CR_TXEN;;			// enable rx and tx
+	UART->UART_CR = UART_CR_RXEN | UART_CR_TXEN;			// enable rx and tx
 
 	uart_fifo_TX = FIFOInit(200);
 	uart_fifo_RX = FIFOInit(200);
@@ -123,7 +112,7 @@ void timer_setup(void){
 	pmc_enable_periph_clk(ID_TC0);
 		
 	TC0->TC_CHANNEL[0].TC_CMR = TC_CMR_TCCLKS_TIMER_CLOCK4 | TC_CMR_CPCTRG| TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC; // set clock to use Mclk/2 and reset on compare match
-	TC0->TC_CHANNEL[0].TC_RC = 650000;
+	TC0->TC_CHANNEL[0].TC_RC = 650000; //Timer reset value
 	TC0->TC_CHANNEL[0].TC_IER = TC_IER_CPCS; //enable interrupt on campare match
 	TC0->TC_CHANNEL[0].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG; // enable and start TC0
 }
@@ -134,7 +123,6 @@ int main (void)
 	sysclk_init(); // this seems to set all internal clocks
 	board_init();  // This seems to set all pins to their correct types for the arduino due
 	
-	
 	adc_setup();
 	uart_setup();
 	timer_setup();
@@ -142,35 +130,18 @@ int main (void)
 	irq_initialize_vectors();
 	NVIC_EnableIRQ(UART_IRQn);
 	NVIC_EnableIRQ(ADC_IRQn);
-	NVIC_EnableIRQ(TC0_IRQn);
-	
+	NVIC_EnableIRQ(TC0_IRQn);	
 	
 	uart_send_string("Starting...\n");
 	while(1){
 		
 	}
-	
-	/* Insert application code here, after the board has been initialized. */
 }
 
 
 void ADC_Handler(void)
 {
-	// Check the ADC conversion status
-// 	if ((adc_get_status(ADC) & ADC_ISR_DRDY) == ADC_ISR_DRDY)
-// 	{
-// 		// Get latest digital data value from ADC and can be used by application
-// 		uint32_t result = adc_get_latest_value(ADC);
-// 		uint8_t fifo_status = FIFOPush(adc_fifo, result & 0x000000FF);
-// 		fifo_status |= FIFOPush(adc_fifo, result & 0x0000FF00);
-// 		
-// 		if(fifo_status != FIFO_OK){
-// 			
-// 			TC0->TC_CHANNEL[0].TC_CCR |= TC_CCR_SWTRG;
-// 			adc_working = 0;
-// 		}
-// 	}
-// 
+	/*this needs to be read here, or the interrupt flag is not cleared*/
 	uint32_t res = ADC->ADC_LCDR;
 	itoa(res,itoabuffer,10);
 	uart_send_string(itoabuffer);
